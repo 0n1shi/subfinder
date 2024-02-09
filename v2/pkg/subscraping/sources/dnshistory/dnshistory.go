7ea12b2c760a49adfd13b2413b2df3121f01d24f
Kazuki Onishi <princeontrojanhorse@gmail.com>
2024-02-09 12:08:54 +0900

    find subdomains from dnshistory

--- v2/pkg/subscraping/sources/dnshistory/dnshistory.go
+++ v2/pkg/subscraping/sources/dnshistory/dnshistory.go
@@ -2,6 +2,8 @@ package dnshistory
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"time"
 
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping"
@@ -15,8 +17,51 @@ type Source struct {
 
 var _ subscraping.Source = &Source{}
 
-func (s *Source) Run(context.Context, string, *subscraping.Session) <-chan subscraping.Result {
+func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Session) <-chan subscraping.Result {
 	results := make(chan subscraping.Result)
+	s.errors = 0
+	s.results = 0
+
+	go func() {
+		defer func(startTime time.Time) {
+			s.timeTaken = time.Since(startTime)
+			close(results)
+		}(time.Now())
+
+		page := 1
+		for {
+			resp, err := session.SimpleGet(ctx, fmt.Sprintf("https://dnshistory.org/subdomains/%d/%s", page, domain))
+			if err != nil {
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+				s.errors++
+				session.DiscardHTTPResponse(resp)
+				return
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+				s.errors++
+				resp.Body.Close()
+				return
+			}
+			resp.Body.Close()
+
+			src := string(body)
+			found := false
+			for _, subdomain := range session.Extractor.Extract(src) {
+				found = true
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
+				s.results++
+			}
+
+			if !found {
+				break
+			}
+			page++
+		}
+	}()
+
 	return results
 }
 
